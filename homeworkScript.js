986b0e018ccd1644c0e1520f657f3050d20db1ac
yhyart <2034068399@qq.com>
2025-03-21 15:42:50 +0800

    Add files via upload

---
+++ homeworkScript.js
@@ -0,0 +1,16 @@
+  function sayHello(){
+        var response=prompt('你的名字是?');
+        alert("哈喽" + response + ",欢迎来到数字猜谜小游戏~");
+      };
+
+function add(a,b){
+  return a + b;
+
+}
+console.log(add(20,30));
+console.log(add(158,20078));
+
+function showTopic(){
+  var x=document.getElementById('demo').style.opacity = 1;
+  x.style.fontSize="25px";
+x.style.color='yellow';}
